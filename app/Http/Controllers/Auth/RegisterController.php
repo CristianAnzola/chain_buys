00ff756c60cf49a490fd61eb2097e02148ef5ed5
CristianAnzola <72874193+CristianAnzola@users.noreply.github.com>
2020-10-15 13:55:15 -0500

    registrate

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -71,6 +71,8 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
-        ]);
+            'genero'    => ['required', 'string', 'max:250'],
+            'tienda'   => 'string'],
+        );
     }
 }
