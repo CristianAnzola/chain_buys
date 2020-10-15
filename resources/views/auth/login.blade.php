76efe0016822f6231c4eb1bc65159fdb2ca158dd
CristianAnzola <72874193+CristianAnzola@users.noreply.github.com>
2020-10-15 09:49:25 -0500

    login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <img src="{{ asset('img/logo.png') }}" width="100px">
+                <div class="card-header">{{ __('CHAIN BUYS) }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
