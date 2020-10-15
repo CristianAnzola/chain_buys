d6d3d0ad862731c74eea24d173e8c4d816dc059c
CristianAnzola <72874193+CristianAnzola@users.noreply.github.com>
2020-10-15 13:40:53 -0500

    register controler

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -53,7 +53,10 @@ class RegisterController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
-        ]);
+            'genero'    => ['required', 'string', 'max:250'],
+            'tienda'   => ['required'], 'string', 'max 250'],
+
+        );
     }
 
     /**
