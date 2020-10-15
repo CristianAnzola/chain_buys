2b7b9b21746eff1dd885c154eb0e3f3ca0444f02
CristianAnzola <72874193+CristianAnzola@users.noreply.github.com>
2020-10-15 08:23:03 -0500

    index

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -45,20 +45,18 @@
             </div>
             <div class="row">
                 <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
-                    <div class="content">                
-                        <div id="carrusel" class="carousel slide" data-ride="carousel">
-                            <div class="carousel-inner">
-                                <div class="carousel-item active">
-                                  <img src="{{ asset('img/Desarrollo-de-Productos-para-Retail1.jpg') }}" class="d-block w-100" alt="..." placeholder="imagen">
-                                </div>
-                                <div class="carousel-item">
-                                  <img src="{{ asset('img/2.jpg') }}" class="d-block w-100" alt="...">
-                                </div>
-                                <div class="carousel-item">
-                                 <img src="{{ asset('img/3.jpg') }}" class="d-block w-100" alt="...">
-                                </div>
+                    <div id="carrusel" class="carousel slide" data-ride="carousel">
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                              <img src="{{ asset('img/Desarrollo-de-Productos-para-Retail1.jpg') }}" class="d-block w-100" alt="..." placeholder="imagen">
                             </div>
-                        </div>    
+                            <div class="carousel-item">
+                              <img src="{{ asset('img/2.jpg') }}" class="d-block w-100" alt="...">
+                            </div>
+                            <div class="carousel-item">
+                             <img src="{{ asset('img/3.jpg') }}" class="d-block w-100" alt="...">
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
