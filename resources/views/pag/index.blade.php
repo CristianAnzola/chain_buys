e1b78cf4ce57e07a23430f73b9de7d60d64e8a64
Kratoz98 <ccanzola3@misena.edu.co>
2020-10-17 03:29:53 -0500

    index 3.0

--- resources/views/pag/index.blade.php
+++ resources/views/pag/index.blade.php
@@ -12,15 +12,16 @@
                     </div>
                     <div class="col-5 col-sm-5 col-md-5 col-lg-5 col-xl-5"></div>
                 </div>
+                <br>
                 <div class="row">
-                    <div class="col-3 col-sm-3 col-md-3 col-lg-3 col-xl-3"></div>
-                    <div class="col-6 col-sm-6 col-md-6 col-lg-6 col-xl-6">
-                        <form class="form-inline my-2 my-lg-0">
-                            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                    <div class="col-2 col-sm-2 col-md-2 col-lg-2 col-xl-2"></div>
+                    <div class="col-8 col-sm-8 col-md-8 col-lg-8 col-xl-8">
+                        <form id="busque" class="form-inline my-2 my-lg-0">
+                            <input id="inbus" class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
                             <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar Producto</button>
                         </form>
                     </div>
-                    <div class="col-3 col-sm-3 col-md-3 col-lg-3 col-xl-3"></div>
+                    <div class="col-2 col-sm-2 col-md-2 col-lg-2 col-xl-2"></div>
                 </div>
             </div>
         </div>
