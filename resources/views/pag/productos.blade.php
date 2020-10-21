61099fe26efe7b23c970bf5b57fe1d26380cc9e6
Kratoz98 <ccanzola3@misena.edu.co>
2020-10-20 19:28:17 -0500

    vista.

--- resources/views/pag/productos.blade.php
+++ resources/views/pag/productos.blade.php
@@ -5,6 +5,17 @@
         <div class="col-1 col-sm-1 col-md-1 col-lg-1 col-xl-1"></div>
         <div class="col-10 col-sm-10 col-md-10 col-lg-10 col-xl-10" id="conten">
             <div class="row">
+                <div class="col-1 col-sm-1 col-md-1 col-lg-1 col-xl-1"></div>
+                <div class="col-2 col-sm-2 col-md-2 col-lg-2 col-xl-2">
+                    <a href="{{ route('home') }}"><img src="{{ asset('img/atras.png') }}" width="30px"></a>
+                </div>
+                <div class="col-1 col-sm-1 col-md-1 col-lg-1 col-xl-1"></div>
+                <div class="col-4 col-sm-4 col-md-4 col-lg-4 col-xl-4" id="centro">
+                    <h2>Productos</h2>
+                </div>
+                <div class="col-4 col-sm-4 col-md-4 col-lg-4 col-xl-4"></div>
+            </div>
+            <div class="row">
                 <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
                     <table id="tablas">
                         <h2>Productos</h2>
